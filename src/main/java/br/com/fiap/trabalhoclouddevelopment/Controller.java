56fe0632fb86a55bc561ea74004a451e7fdedbc3
Jonathas Garcia <jonathas-garcia>
2021-05-15 23:28:16 -0300

    Ajuste

--- src/main/java/br/com/fiap/trabalhoclouddevelopment/Controller.java
+++ src/main/java/br/com/fiap/trabalhoclouddevelopment/Controller.java
@@ -1,5 +1,6 @@
 package br.com.fiap.trabalhoclouddevelopment;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/teste")
 public class Controller {
 
+    @GetMapping
     public String test(){
         return "OK";
     }
